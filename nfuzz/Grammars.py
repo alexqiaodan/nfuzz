14f566df52dcd14aa1dc9a65986633201283db35
alexqiaodan <alexqiaodan@163.com>
2022-05-04 18:01:08 +0800

    Update Grammars.py

--- nfuzz/Grammars.py
+++ nfuzz/Grammars.py
@@ -80,6 +80,33 @@ EXPR_GRAMMAR = {
         ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 }
 
+
+'''算数表达式Demo规则'''
+EXPR_DEMO_GRAMMAR = {
+    "<start>":
+        ["<expr>"],
+
+    "<expr>":
+        ["<term> + <expr>", "<term> - <expr>", "<term>"],
+
+    "<term>":
+        ["<factor> * <term>", "<factor> / <term>", "<factor>"],
+
+    "<factor>":
+        ["+<factor>",
+         "-<factor>",
+         "(<expr>)",
+         "<integer>.<integer>",
+         "<integer>"],
+
+    "<integer>":
+        ["<digit><integer>", "<digit>"],
+
+    "<digit>":
+        ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+}
+
+
 '''url规则语法'''
 URL_GRAMMAR = {
     "<start>":
